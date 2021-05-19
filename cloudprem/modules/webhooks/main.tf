c5375eea5db1e70cf8a3d6474198d18a6ce94ced
zetas <ddv@qubitlogic.net>
2021-05-18 23:58:50 -0400

    Cleaning up code and moving static helm values to external values.yaml file for include. Adding config settings for kafka to allow frontegg helm chart to work. Adding mongo and redis helm charts to install on-cluster instead of using documentdb and elasticache-redis.

--- cloudprem/modules/webhooks/main.tf
+++ cloudprem/modules/webhooks/main.tf
@@ -1,23 +1,7 @@
 data "aws_kms_key" "rds" {
   key_id = var.rds_kms_key_id
 }
-data "kubernetes_secret" "frontegg" {
-  metadata {
-    name = "frontegg-credentials"
-    namespace = "default"
-  }
-}
-//resource "kubernetes_config_map" "mongo_configmap" {
-//
-//  metadata {
-//    name = "mongo-tls"
-//    namespace = "default"
-//  }
-//
-//  data = {
-//    "rds-combined-ca-bundle.pem" = file("./modules/webhooks/vendor/rds-combined-ca-bundle.pem")
-//  }
-//}
+
 resource "aws_security_group" "this" {
   name        = "${var.name}-webhooks"
   description = "Webhooks SG. Allows access to the kafka cluster"
@@ -65,15 +49,15 @@ resource "aws_security_group_rule" "ingress_cidr_blocks" {
   type              = "ingress"
 }
 
-//resource "aws_cloudwatch_log_group" "logs" {
-//  name = "msk_broker_logs"
-//}
-//
-//resource "aws_s3_bucket" "bucket" {
-//  bucket = "${var.name}-msk-broker-logs-bucket"
-//  acl    = "private"
-//}
+resource "aws_msk_configuration" "this" {
+  kafka_versions = ["2.7.0"]
+  name           = "${var.name}-kafka-config"
 
+  server_properties = <<PROPERTIES
+auto.create.topics.enable = true
+delete.topic.enable = true
+PROPERTIES
+}
 
 resource "aws_msk_cluster" "this" {
   cluster_name           = "${var.name}-kafka"
@@ -87,9 +71,15 @@ resource "aws_msk_cluster" "this" {
     security_groups = [join("", aws_security_group.this.*.id)]
   }
 
-//  encryption_info {
-//    encryption_at_rest_kms_key_arn = data.aws_kms_key.rds.arn
-//  }
+  configuration_info {
+    arn = aws_msk_configuration.this.arn
+    revision = aws_msk_configuration.this.latest_revision
+  }
+  encryption_info {
+    encryption_in_transit {
+      client_broker = "TLS_PLAINTEXT"
+    }
+  }
 
   open_monitoring {
     prometheus {
@@ -102,76 +92,41 @@ resource "aws_msk_cluster" "this" {
     }
   }
 
-//  logging_info {
-//    broker_logs {
-//      cloudwatch_logs {
-//        enabled   = true
-//        log_group = aws_cloudwatch_log_group.logs.name
-//      }
-//      s3 {
-//        enabled = true
-//        bucket  = aws_s3_bucket.bucket.id
-//        prefix  = "logs/msk-"
-//      }
-//    }
-//  }
-
   tags = var.tags
 }
-resource "aws_docdb_subnet_group" "default" {
-  name       = "${var.name}-mongo-subnet_group"
-  subnet_ids = var.subnet_ids
 
-  tags = var.tags
-}
-resource "aws_docdb_cluster_parameter_group" "disable_tls" {
-  family      = "docdb4.0"
-  name        = "${var.name}-mongo-param-grop"
-  description = "docdb cluster parameter group"
+resource "helm_release" "mongodb" {
 
-  parameter {
-    name  = "tls"
-    value = "disabled"
+  name = "frontegg-documents"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart = "mongodb"
+  version = "10.15.2"
+
+  set {
+    name = "auth.enabled"
+    value = "false"
   }
 }
 
-resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 1
-  identifier         = "${var.name}-mongo-${count.index}"
-  cluster_identifier = aws_docdb_cluster.this.id
-  instance_class     = "db.t3.medium"
+resource "helm_release" "redis" {
 
+  name = "frontegg-kvstore"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart = "redis"
+  version = "14.1.1"
 
-  tags = var.tags
-}
-
-resource "aws_docdb_cluster" "this" {
-  cluster_identifier = "${var.name}-mongo"
-  vpc_security_group_ids = [aws_security_group.this.id]
-  master_username    = "foo"
-  master_password    = "barbut8chars"
-  db_subnet_group_name = aws_docdb_subnet_group.default.name
-  skip_final_snapshot = true
-  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.disable_tls.name
-
-  tags = var.tags
-}
-
-resource "aws_elasticache_subnet_group" "default" {
-  name       = "${var.name}-redis-subnet-group"
-  subnet_ids = var.subnet_ids
-}
-
-resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "${var.name}-redis"
-  engine               = "redis"
-  node_type            = "cache.t3.micro"
-  subnet_group_name = aws_elasticache_subnet_group.default.name
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis5.0"
-  engine_version       = "5.0.6"
-  security_group_ids = [aws_security_group.this.id]
-  port                 = 6379
+  set {
+    name = "auth.enabled"
+    value = "false"
+  }
+  set {
+    name = "tls.authClients"
+    value = "false"
+  }
+  set {
+    name = "architecture"
+    value = "standalone"
+  }
 }
 
 
@@ -181,153 +136,120 @@ resource "helm_release" "frontegg" {
     local_file.default_helmignore,
     local_file.api_helmignore,
     local_file.event_helmignore,
-    local_file.webhook_helmignore
+    local_file.webhook_helmignore,
+    helm_release.mongodb,
+    helm_release.redis
   ]
 
-  name  = "connectivity"
+  name  = "frontegg"
   chart = "${path.module}/charts/connectivity"
 
   namespace = "default"
 
-  timeout = 600
   reuse_values = true
-//  force_update = true
-//  cleanup_on_fail = true
+//  wait       = false
 
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+
+  // - Frontegg Auth - //
   set_sensitive {
     name = "event-service.frontegg.clientId"
-    value = "dummyid"
+    value = var.frontegg_client_id
   }
   set_sensitive {
     name = "event-service.frontegg.apiKey"
-    value = "dummyid"
+    value = var.frontegg_api_key
+  }
+  set_sensitive {
+    name = "api-gateway.frontegg.authenticationPublicKey"
+    value = var.frontegg_secret.data.pubkey
   }
 
+  // - Kafka - //
   set {
     name  = "webhook-service.messageBroker.brokerList"
-    value = replace(aws_msk_cluster.this.zookeeper_connect_string,",","\\,")
+    value = replace(aws_msk_cluster.this.bootstrap_brokers,",","\\,")
   }
   set {
     name  = "event-service.messageBroker.brokerList"
-    value = replace(aws_msk_cluster.this.zookeeper_connect_string,",","\\,")
+    value = replace(aws_msk_cluster.this.bootstrap_brokers,",","\\,")
   }
   set {
     name  = "integrations-service.messageBroker.brokerList"
-    value = replace(aws_msk_cluster.this.zookeeper_connect_string,",","\\,")
+    value = replace(aws_msk_cluster.this.bootstrap_brokers,",","\\,")
   }
   set {
     name  = "connectors-worker.messageBroker.brokerList"
-    value = replace(aws_msk_cluster.this.zookeeper_connect_string,",","\\,")
+    value = replace(aws_msk_cluster.this.bootstrap_brokers,",","\\,")
   }
 
-  set {
-    name = "webhook-service.mongo.connectionString"
-    value = "mongodb://foo:barbut8chars@${aws_docdb_cluster.this.endpoint}:${aws_docdb_cluster_instance.cluster_instances.0.port}/?retryWrites=false"
-  }
-  set {
-    name = "integrations-service.mongo.connectionString"
-    value = "mongodb://foo:barbut8chars@${aws_docdb_cluster.this.endpoint}:${aws_docdb_cluster_instance.cluster_instances.0.port}/?retryWrites=false"
-  }
-
-  set {
-    name = "webhook-service.redis.host"
-    value = aws_elasticache_cluster.redis.cache_nodes.0.address
-  }
-  set {
-    name = "event-service.redis.host"
-    value = aws_elasticache_cluster.redis.cache_nodes.0.address
-  }
-  set {
-    name = "connectors-worker.redis.host"
-    value = aws_elasticache_cluster.redis.cache_nodes.0.address
-  }
-
+  // - MySQL - //
   set {
     name = "webhook-service.mysql.host"
     value = var.rds_address
-  }
-  set {
-    name = "webhook-service.mysql.username"
-    value = var.rds_user
-  }
-
-  set_sensitive {
-    name = "webhook-service.mysql.password"
-    value = var.rds_pass
   }
   set {
     name = "event-service.database.host"
     value = var.rds_address
   }
   set {
+    name = "webhook-service.mysql.username"
+    value = var.rds_user
+  }
+  set {
     name = "event-service.database.username"
     value = var.rds_user
   }
-
+  set_sensitive {
+    name = "webhook-service.mysql.password"
+    value = var.rds_pass
+  }
   set_sensitive {
     name = "event-service.database.password"
     value = var.rds_pass
   }
 
-  set {
-    name = "api-gateway.frontegg.authenticationPublicKey"
-    value = data.kubernetes_secret.frontegg.data.pubkey
-  }
-
-  set {
+  // - Frontegg Docker Auth - //
+  set_sensitive {
     name = "frontegg.images.username"
-    value = data.kubernetes_secret.frontegg.data.username
+    value = var.frontegg_secret.data.username
   }
   set_sensitive {
     name = "frontegg.images.password"
-    value = data.kubernetes_secret.frontegg.data.password
-  }
-
-  set {
-    name = "integrations-service.frontegg.slack.encryptionKey"
-    value = "dummy"
-  }
-  set {
-    name = "connectors-worker.frontegg.channels"
-    value = "slack"
-  }
-  set {
-    name = "connectors-worker.frontegg.emails.provider"
-    value = "sendgrid"
-  }
-  set {
-    name = "connectors-worker.frontegg.emails.sendgrid.apiKey"
-    value = "dummyval"
+    value = var.frontegg_secret.data.password
   }
 
 }
+# Necessary hackery to prevent generated terraform/terragrunt files from being included by helm and blowing up the deploy.
 resource local_file default_helmignore {
-  content     = file("./modules/webhooks/helmignore")
-  filename = "./modules/webhooks/charts/connectivity/.helmignore"
+  content     = file("${path.module}/helmignore")
+  filename = "${path.module}/charts/connectivity/.helmignore"
   file_permission = "0644"
 }
 resource local_file event_helmignore {
-  content     = file("./modules/webhooks/helmignore")
-  filename = "./modules/webhooks/charts/connectivity/charts/event-service/.helmignore"
+  content     = file("${path.module}/helmignore")
+  filename = ".${path.module}/charts/connectivity/charts/event-service/.helmignore"
   file_permission = "0644"
 }
 resource local_file api_helmignore {
-  content     = file("./modules/webhooks/helmignore")
-  filename = "./modules/webhooks/charts/connectivity/charts/api-gateway/.helmignore"
+  content     = file("${path.module}/helmignore")
+  filename = "${path.module}/charts/connectivity/charts/api-gateway/.helmignore"
   file_permission = "0644"
 }
 resource local_file webhook_helmignore {
-  content     = file("./modules/webhooks/helmignore")
-  filename = "./modules/webhooks/charts/connectivity/charts/webhook-service/.helmignore"
+  content     = file("${path.module}/helmignore")
+  filename = "${path.module}/charts/connectivity/charts/webhook-service/.helmignore"
   file_permission = "0644"
 }
 resource local_file connectors_helmignore {
-  content     = file("./modules/webhooks/helmignore")
-  filename = "./modules/webhooks/charts/connectivity/charts/connectors-worker/.helmignore"
+  content     = file("${path.module}/helmignore")
+  filename = "${path.module}/charts/connectivity/charts/connectors-worker/.helmignore"
   file_permission = "0644"
 }
 resource local_file integrations_helmignore {
-  content     = file("./modules/webhooks/helmignore")
-  filename = "./modules/webhooks/charts/connectivity/charts/integrations-service/.helmignore"
+  content     = file("${path.module}/helmignore")
+  filename = "${path.module}/charts/connectivity/charts/integrations-service/.helmignore"
   file_permission = "0644"
 }
