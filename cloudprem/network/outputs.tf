53e00959d9c875bc727669f8e317d810f663fcd7
David Della Vecchia <ddv@dozuki.com>
2021-12-19 07:06:17 -0500

    Removing old terraform code and moving new refactor in place
    Signed-off-by: David Della Vecchia <ddv@dozuki.com>

--- cloudprem/network/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = local.vpc_id
-}
-output "azs_count" {
-  value = local.azs_count
-}
--- physlog/logical/static/frontegg-db.sql
+++
@@ -1,1 +0,0 @@
-create database if not exists frontegg_events; create database if not exists frontegg_webhooks; update sites.site_config set value = 'true' where name = 'feature-webhooks';
