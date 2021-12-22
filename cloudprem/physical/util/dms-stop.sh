62e01308ffae9da6cd0fc4e8a3b9cbf52f5f4742
David Della Vecchia <ddv@dozuki.com>
2021-12-22 01:13:12 -0500

    Refactor DMS stop script to prevent infinite loop on unexpected return value.
    Signed-off-by: David Della Vecchia <ddv@dozuki.com>

--- cloudprem/physical/util/dms-stop.sh
+++ cloudprem/physical/util/dms-stop.sh
@@ -4,25 +4,35 @@ TRIGGER="$1"
 AWS_REGION="$2"
 AWS_PROFILE="$3"
 
-function checkForStoppedDMS() {
+function getDMSStatus() {
   aws dms describe-replication-tasks --filter Name=replication-task-arn,Values="$TRIGGER" --without-settings --region "$AWS_REGION" --profile "$AWS_PROFILE" |jq --raw-output '.[][0]["Status"]'
+}
+
+function stopDMS() {
+  local STATUS
+
+  STATUS=$(getDMSStatus)
+   if [[ "$STATUS" == "running" ]]; then
+     echo -e "Stopping DMS Task..."
+     aws dms stop-replication-task --replication-task-arn "$TRIGGER" --region "$AWS_REGION" --profile "$AWS_PROFILE" > /dev/null
+     waitforStoppedDMS
+   else
+     echo -e "DMS Already Stopped."
+   fi
 }
 
 function waitforStoppedDMS() {
   echo "Waiting for the DMS task to stop..."
 
-   local STATUS
+  local STATUS
 
-   STATUS=$(checkForStoppedDMS)
-   until [[ "$STATUS" == "stopped" ]]; do
-      sleep 10
-      STATUS=$(checkForStoppedDMS)
-   done
+  STATUS=$(getDMSStatus)
+  while [[ "$STATUS" == "stopping" ]]; do
+    sleep 10
+    STATUS=$(getDMSStatus)
+  done
 
-   echo -e "DMS Task Stopped Successfully."
+  echo -e "DMS Task Stopped Successfully."
 }
 
-aws dms stop-replication-task --replication-task-arn "$TRIGGER" --region "$AWS_REGION" --profile "$AWS_PROFILE" > /dev/null
-
-waitforStoppedDMS
-
+stopDMS
