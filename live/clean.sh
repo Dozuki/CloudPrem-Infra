77f294615d8e88c4c561fc770a4db8c7ceadecfa
David Della Vecchia <ddv@dozuki.com>
2022-08-04 13:25:30 -0400

    try dynamic matrix
    Signed-off-by: David Della Vecchia <ddv@dozuki.com>

--- live/clean.sh
+++ live/clean.sh
@@ -11,28 +11,28 @@ cleanPrefixes() {
     for p in ${prefixes[@]}; do
         echo "Cleaning prefix $p"
         cleanTerraFiles
-        for region in $(ls); do
-             if [[ $region != "account.hcl" ]]; then
-                echo "Clearing k8 auth from state for $region"
-                pushd $region
-                for env in ${envs[@]}; do
-                    echo "Clearing $env"
-                    pushd $env/physical
-                    TG_STATE_PREFIX="test/$p/" terragrunt state rm "module.eks_cluster.kubernetes_config_map.aws_auth[0]"
-                    popd
-                done
-                popd
-             fi
-        done
+#         for region in $(ls); do
+#              if [[ $region != "account.hcl" ]]; then
+#                 echo "Clearing k8 auth from state for $region"
+#                 pushd $region
+#                 for env in ${envs[@]}; do
+#                     echo "Clearing $env"
+#                     pushd $env/physical
+#                     TG_STATE_PREFIX="test/$p/" terragrunt state rm "module.eks_cluster.kubernetes_config_map.aws_auth[0]"
+#                     popd
+#                 done
+#                 popd
+#              fi
+#         done
         SKIP_LOGICAL=true TG_STATE_PREFIX="test/$p/" terragrunt run-all destroy --terragrunt-non-interactive
     done
 }
-echo "Cleaning standard"
-cd standard
-cleanPrefixes
-cd ../gov
-cleanPrefixes
-cd ..
+# echo "Cleaning standard"
+# cd standard
+# cleanPrefixes
+# cd ../gov
+# cleanPrefixes
+# cd ..
 
 for region in $(ls standard/); do
  if [[ $region != "account.hcl" ]]; then
@@ -44,3 +44,5 @@ done
 
 echo "Cleaning Gov"
 awsweeper --region us-gov-west-1 --profile gov --force tests/nuke.yaml
+
+cloud-nuke aws --exclude-resource-type cloudwatch-loggroup --exclude-resource-type lambda --exclude-resource-type iam-role --force --config tests/cloud-nuke.yaml
