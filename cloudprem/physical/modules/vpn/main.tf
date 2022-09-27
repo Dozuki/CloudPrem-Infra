64b8650b7a31c57f6440cb58141ea49816269023
David Della Vecchia <ddv@dozuki.com>
2022-09-27 17:00:00 -0400

    Refactor the ssm prefix to remove duplicate dozuki references and be more semantic
    Signed-off-by: David Della Vecchia <ddv@dozuki.com>

--- cloudprem/physical/modules/vpn/main.tf
+++ cloudprem/physical/modules/vpn/main.tf
@@ -17,12 +17,13 @@ terraform {
 locals {
   identifier = var.identifier == "" ? "dozuki-${var.environment}" : "${var.identifier}-dozuki-${var.environment}"
 
-  ssm_prefix = "/dozuki/${local.identifier}/${data.aws_region.current.name}"
+  ssm_prefix = "/dozuki/${coalesce(var.identifier, "general")}/${var.environment}/${data.aws_region.current.name}"
 
+  # Tags for all resources. If you add a tag, it must never be blank.
   tags = {
     Terraform   = "true"
     Project     = "Dozuki"
-    Identifier  = var.identifier
+    Identifier  = coalesce(var.identifier, "NA")
     Environment = var.environment
   }
 }
