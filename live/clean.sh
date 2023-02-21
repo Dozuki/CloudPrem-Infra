563b9c6ee9e26707e7aba66d3331f3b2a2469d3b
David Della Vecchia <ddv@dozuki.com>
2023-02-21 17:03:45 -0500

    Refactor clean script to be more efficient and reliable
    Signed-off-by: David Della Vecchia <ddv@dozuki.com>

--- live/clean.sh
+++ live/clean.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 prefixes=("default" "ha" "vpn" "havpn")
 envs=("min" "bi" "hooks" "full")
@@ -7,45 +8,57 @@ cleanTerraFiles() {
     find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;
 }
 
+cleanK8Auth() {
+  for region in *; do
+    if [[ $region != "account.hcl" ]]; then
+      echo "Clearing k8 auth from state for $region"
+      pushd "$region" || exit
+      for env in "${envs[@]}"; do
+        echo "Clearing $env"
+        pushd "$env/physical" || exit
+        TG_STATE_PREFIX="test/$p/" terragrunt state rm "module.eks_cluster.kubernetes_config_map.aws_auth[0]"
+        popd || exit
+      done
+      popd || exit
+    fi
+  done
+}
+
 cleanPrefixes() {
-    for p in ${prefixes[@]}; do
+    for p in "${prefixes[@]}"; do
         echo "Cleaning prefix $p"
         cleanTerraFiles
-#         for region in $(ls); do
-#              if [[ $region != "account.hcl" ]]; then
-#                 echo "Clearing k8 auth from state for $region"
-#                 pushd $region
-#                 for env in ${envs[@]}; do
-#                     echo "Clearing $env"
-#                     pushd $env/physical
-#                     TG_STATE_PREFIX="test/$p/" terragrunt state rm "module.eks_cluster.kubernetes_config_map.aws_auth[0]"
-#                     popd
-#                 done
-#                 popd
-#              fi
-#         done
+        # If you're having issues with stuck kubernetes auth configmaps (which happens sometimes on an un-clean delete)
+        # uncomment this function invocation and re-run the script.
+        #cleanK8Auth
         SKIP_LOGICAL=true TG_STATE_PREFIX="test/$p/" terragrunt run-all destroy --terragrunt-non-interactive
     done
 }
+
+if [ ! -d standard ] || [ ! -d gov ]; then
+  ./generate_live_env.sh
+fi
+
 echo "Cleaning standard"
-pushd standard
+pushd standard || exit
 cleanPrefixes
-popd
-pushd gov
-cleanPrefixes
-popd
-
-
-for region in $(ls standard/); do
- if [[ $region != "account.hcl" ]]; then
+for region in *; do
+ if [ -d "$region" ]; then
     echo "Cleaning $region"
-    awsweeper --region $region --force tests/awsweeper.yaml
+    awsweeper --region "$region" --force ../tests/awsweeper.yaml
 
  fi
 done
+cloud-nuke aws --exclude-resource-type cloudwatch-loggroup --exclude-resource-type lambda --exclude-resource-type iam-role --force --config ../tests/cloud-nuke.yaml
+popd || exit
 
-echo "Cleaning Gov"
-awsweeper --region us-gov-west-1 --profile gov --force tests/awsweeper.yaml
+echo "Cleaning gov"
+pushd gov || exit
+cleanPrefixes
+awsweeper --region us-gov-west-1 --profile gov --force ../tests/awsweeper.yaml
+AWS_PROFILE=gov cloud-nuke aws --region us-gov-west-1 --exclude-resource-type cloudwatch-loggroup --exclude-resource-type lambda --exclude-resource-type macie-member --force --config ../tests/cloud-nuke.yaml
+popd || exit
 
-cloud-nuke aws --exclude-resource-type cloudwatch-loggroup --exclude-resource-type lambda --exclude-resource-type iam-role --force --config tests/cloud-nuke.yaml
-AWS_PROFILE=gov cloud-nuke aws --region us-gov-west-1 --exclude-resource-type cloudwatch-loggroup --exclude-resource-type lambda --exclude-resource-type macie-member --force --config tests/cloud-nuke.yaml
+
+
+
