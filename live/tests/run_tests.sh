816846fc7ddd30a15ab545850401720629ad945e
David Della Vecchia <ddv@dozuki.com>
2023-03-08 13:33:28 -0500

    Update test runner to ensure live environment exists

--- live/tests/run_tests.sh
+++ live/tests/run_tests.sh
@@ -2,9 +2,17 @@
 
 date=$(date +%Y-%m-%d-%T)
 
-cd src
+# Ensure the live environment exists
+if [ ! -d ../standard ] || [ ! -d ../gov ]; then
+  pushd ../
+  ./generate_live_env.sh
+  popd || exit
+fi
+# Clear all existing terragrunt config/terraform files in live environment
+find ../ -name '.terra*' -exec rm -fr {} \;
+cd src || exit
 go test -v -count 1 -timeout 3000m |terratest_log_parser --outputdir "test_output-${date}"
 cd ../../
 ./clean.sh
-cd tests
+cd tests || exit
 say "tests done"
