0b0785b3071848b371847b121a969222b3ebaa64
David Della Vecchia <ddv@dozuki.com>
2023-03-08 13:31:31 -0500

    Refactor live environment generation to add account number customization since we are no longer using 1 test account

--- live/.skel/partitions/gov/account.hcl
+++
@@ -1,6 +0,0 @@
-# Set account-wide variables. These are automatically pulled in to configure the remote state bucket in the root
-# terragrunt.hcl configuration.
-locals {
-  aws_account_id = "446787640263"
-  aws_profile    = "gov"
-}
--- live/generate_live_env.sh
+++ live/generate_live_env.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
+PARTITIONS_PATH=../.skel/partitions
+
 for partition in standard gov; do
   if [ -d "$partition" ]; then
     rm -fr "$partition"
   fi
   mkdir $partition
   pushd $partition || exit
-  cp ../.skel/partitions/$partition/account.hcl .
+  if [ -f $PARTITIONS_PATH/$partition/account.hcl ]; then
+    cp $PARTITIONS_PATH/$partition/account.hcl .
+  else
+    cat << EOF > $PARTITIONS_PATH/$partition/account.hcl
+locals {
+  aws_account_id = "123456789"
+  aws_profile    = "your_profile"
+}
+EOF
+    echo "Update $PARTITIONS_PATH/$partition/account.hcl to reflect your test account info and re-run this script."
+    exit
+  fi
   while IFS= read -r region
   do
       echo "Creating live environments for $region"
@@ -22,6 +35,6 @@ locals {
 }
 EOF
       popd || exit
-  done < <(grep -v '^ *#' < ../.skel/partitions/$partition/regions.txt)
+  done < <(grep -v '^ *#' < $PARTITIONS_PATH/$partition/regions.txt)
   popd || exit
 done
