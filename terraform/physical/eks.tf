cc510064a8433d38ae33e8e0b24ec4fbf48e456d
David Della Vecchia <ddv@dozuki.com>
2023-06-09 19:30:48 -0400

    Add additional S3 iam permissions to support changes in s3 upload in the app

--- terraform/physical/eks.tf
+++ terraform/physical/eks.tf
@@ -143,7 +143,12 @@ data "aws_iam_policy_document" "eks_worker" {
       "s3:GetObjectAcl",
       "s3:ListBucket",
       "s3:ListObjectsV2",
-      "s3:CopyObject"
+      "s3:CopyObject",
+      "s3:DeleteObjectTagging",
+      "s3:ReplicateTags",
+      "s3:PutObjectVersionTagging",
+      "s3:PutObjectTagging",
+      "s3:DeleteObjectVersionTagging"
     ]
 
     resources = [
