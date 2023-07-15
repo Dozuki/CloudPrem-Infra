243c6be7aa3bb2a70720f378d4010862d25ad5eb
David Della Vecchia <ddv@dozuki.com>
2023-07-15 13:52:12 -0400

    Remove dms role and permission setup from bootstrap module since it's handled in the main module now

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -18,8 +18,6 @@ locals {
   }
 }
 
-data "aws_partition" "current" {}
-
 resource "aws_ssm_parameter" "customer_ids" {
   for_each = var.customer_id_parameters
 
@@ -29,58 +27,6 @@ resource "aws_ssm_parameter" "customer_ids" {
   value       = each.value
 
   tags = local.tags
-}
-
-data "aws_iam_policy_document" "dms_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      identifiers = ["dms.amazonaws.com"]
-      type        = "Service"
-    }
-  }
-}
-
-resource "aws_iam_role" "dms-access-for-endpoint" {
-  count              = var.dms_setup ? 1 : 0
-  assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
-  name               = "dms-access-for-endpoint"
-}
-
-resource "aws_iam_role_policy_attachment" "dms-access-for-endpoint-AmazonDMSRedshiftS3Role" {
-  count = var.dms_setup ? 1 : 0
-
-  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AmazonDMSRedshiftS3Role"
-  role       = aws_iam_role.dms-access-for-endpoint[0].name
-}
-
-resource "aws_iam_role" "dms-cloudwatch-logs-role" {
-  count = var.dms_setup ? 1 : 0
-
-  assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
-  name               = "dms-cloudwatch-logs-role"
-}
-
-resource "aws_iam_role_policy_attachment" "dms-cloudwatch-logs-role-AmazonDMSCloudWatchLogsRole" {
-  count = var.dms_setup ? 1 : 0
-
-  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AmazonDMSCloudWatchLogsRole"
-  role       = aws_iam_role.dms-cloudwatch-logs-role[0].name
-}
-
-resource "aws_iam_role" "dms-vpc-role" {
-  count = var.dms_setup ? 1 : 0
-
-  assume_role_policy = data.aws_iam_policy_document.dms_assume_role.json
-  name               = "dms-vpc-role"
-}
-
-resource "aws_iam_role_policy_attachment" "dms-vpc-role-AmazonDMSVPCManagementRole" {
-  count = var.dms_setup ? 1 : 0
-
-  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AmazonDMSVPCManagementRole"
-  role       = aws_iam_role.dms-vpc-role[0].name
 }
 
 
