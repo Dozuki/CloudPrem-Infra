5a8e45bf772f4b5d0a1795c76cf06daa7193f83c
David Della Vecchia <ddv@dozuki.com>
2023-08-26 05:11:33 -0400

    Add documentation comments to utility scripts

--- terraform/physical/util/create-s3-batch.sh
+++ terraform/physical/util/create-s3-batch.sh
@@ -1,4 +1,31 @@
 #!/usr/bin/env bash
+# AWS S3 Batch Replication Script
+#
+# Purpose:
+# This script facilitates the creation of an S3 batch replication job.
+# It sets up replication for objects in a source bucket, generating reports for the tasks.
+#
+# Main Functionality:
+# - Accepts several input arguments, including the AWS logging bucket, source bucket, replication role ARN, account ID, partition, and optionally an AWS profile.
+# - Configures the AWS CLI command based on the provided profile, if specified.
+# - Initiates an S3 batch job to replicate objects, focusing on those that are eligible for replication or have failed replication statuses.
+# - Outputs reports regarding the replication tasks to a specified logging bucket.
+#
+# Usage:
+# ./create-s3-batch.sh <AWS_LOGGING_BUCKET> <AWS_SOURCE_BUCKET> <AWS_REPLICATION_ROLE> <AWS_ACCOUNT> <AWS_PARTITION> [AWS_PROFILE]
+#
+# Required Arguments:
+# AWS_LOGGING_BUCKET:  The S3 bucket where replication reports will be stored.
+# AWS_SOURCE_BUCKET:   The S3 bucket containing objects to be replicated.
+# AWS_REPLICATION_ROLE: The ARN of the role to be used for replication tasks.
+# AWS_ACCOUNT:         The AWS account ID.
+# AWS_PARTITION:       The AWS partition (e.g., aws, aws-cn, aws-us-gov).
+#
+# Optional Argument:
+# AWS_PROFILE:         The AWS CLI profile to use. If not provided, the default profile or the AWS_PROFILE environment variable is used.
+#
+# Note:
+# Ensure AWS CLI is set up and permissions for S3 and S3 Control are correctly configured for the provided profile.
 
 set -euo pipefail
 
