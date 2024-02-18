eb5d8459069e070e1d65b2382be3691494d5202b
David Della Vecchia <ddv@dozuki.com>
2024-02-17 21:04:38 -0500

    Remove filter from batch job

--- terraform/physical/util/create-s3-batch.sh
+++ terraform/physical/util/create-s3-batch.sh
@@ -19,7 +19,7 @@ $AWS_PREFIX aws s3control create-job \
   --account-id "$AWS_ACCOUNT" \
   --operation '{"S3ReplicateObject":{}}' \
   --report "{\"Bucket\":\"$AWS_LOGGING_BUCKET\",\"Prefix\":\"batch-replication-report\", \"Format\":\"Report_CSV_20180820\",\"Enabled\":true,\"ReportScope\":\"AllTasks\"}" \
-  --manifest-generator "{\"S3JobManifestGenerator\": {\"SourceBucket\": \"arn:$AWS_PARTITION:s3:::$AWS_SOURCE_BUCKET\", \"EnableManifestOutput\": false, \"Filter\": {\"EligibleForReplication\": true, \"ObjectReplicationStatuses\": [\"NONE\",\"FAILED\"]}}}" \
+  --manifest-generator "{\"S3JobManifestGenerator\": {\"SourceBucket\": \"arn:$AWS_PARTITION:s3:::$AWS_SOURCE_BUCKET\", \"EnableManifestOutput\": false}}" \
   --priority 1 \
   --role-arn "$AWS_REPLICATION_ROLE" \
   --no-confirmation-required
