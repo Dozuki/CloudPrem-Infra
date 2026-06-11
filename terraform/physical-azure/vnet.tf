ac71e531c3ce59207b3d5d90ab411414d5dbdea9
David Della Vecchia <ddv@dozuki.com>
2026-06-11 12:16:57 -0400

    feat(azure): vnet, aks/mysql subnets, mysql private dns zone

---
+++ terraform/physical-azure/vnet.tf
@@ -0,0 +1,45 @@
+resource "azurerm_virtual_network" "this" {
+  name                = "${local.identifier}-vnet"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  address_space       = [var.vnet_cidr]
+  tags                = local.tags
+}
+
+# Node subnet. Pods use the CNI overlay CIDR, not this subnet.
+resource "azurerm_subnet" "aks" {
+  name                 = "${local.identifier}-aks"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [cidrsubnet(var.vnet_cidr, 4, 0)] # e.g. 10.10.0.0/20
+}
+
+# MySQL Flexible Server requires a delegated subnet.
+resource "azurerm_subnet" "mysql" {
+  name                 = "${local.identifier}-mysql"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [cidrsubnet(var.vnet_cidr, 8, 16)] # e.g. 10.10.16.0/24
+
+  delegation {
+    name = "mysql-flexible-server"
+
+    service_delegation {
+      name    = "Microsoft.DBforMySQL/flexibleServers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
+
+resource "azurerm_private_dns_zone" "mysql" {
+  name                = local.mysql_private_dns_zone
+  resource_group_name = azurerm_resource_group.this.name
+  tags                = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "mysql" {
+  name                  = "${local.identifier}-mysql"
+  resource_group_name   = azurerm_resource_group.this.name
+  private_dns_zone_name = azurerm_private_dns_zone.mysql.name
+  virtual_network_id    = azurerm_virtual_network.this.id
+}
