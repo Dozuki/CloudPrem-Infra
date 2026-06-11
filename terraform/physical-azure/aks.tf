0ee050f6e832afe252561e16c1288e88f1fd7008
David Della Vecchia <ddv@dozuki.com>
2026-06-11 12:35:55 -0400

    feat(azure): aks cluster with workload identity, oidc, acr pull

---
+++ terraform/physical-azure/aks.tf
@@ -0,0 +1,60 @@
+resource "azurerm_kubernetes_cluster" "this" {
+  name                = "${local.identifier}-aks"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  dns_prefix          = local.identifier
+  kubernetes_version  = var.aks_kubernetes_version
+
+  sku_tier                  = "Standard"
+  automatic_upgrade_channel = "patch"
+
+  oidc_issuer_enabled       = true
+  workload_identity_enabled = true
+
+  default_node_pool {
+    name                 = "system"
+    vm_size              = var.aks_node_vm_size
+    auto_scaling_enabled = true
+    min_count            = var.aks_node_count_min
+    max_count            = var.aks_node_count_max
+    vnet_subnet_id       = azurerm_subnet.aks.id
+    tags                 = local.tags
+
+    upgrade_settings {
+      max_surge = "10%"
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin      = "azure"
+    network_plugin_mode = "overlay"
+    pod_cidr            = "192.168.0.0/16"
+    service_cidr        = "172.16.0.0/16"
+    dns_service_ip      = "172.16.0.10"
+    load_balancer_sku   = "standard"
+    outbound_type       = "loadBalancer"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+  }
+
+  tags = local.tags
+
+  lifecycle {
+    ignore_changes = [kubernetes_version]
+  }
+}
+
+# Image pulls come from the customer's ACR (synced from GHCR at deploy time).
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  count = var.acr_id != "" ? 1 : 0
+
+  scope                = var.acr_id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
+}
